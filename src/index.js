8ec668f7309834a7dfc8d1e056b8a993bd4af306
Mihai Mitricel <mihaimitricel24@gmail.com>
2023-02-17 09:23:54 +0200

    ex 6

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Hello } from './Hello';
+import { App } from './App';
+const helloworld = <App />
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(helloworld);
